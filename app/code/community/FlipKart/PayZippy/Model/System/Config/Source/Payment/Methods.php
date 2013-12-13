546cf6afc9ee6292112735cc62c81de86a61c0b0
Chandan Bansal <chandan.bansal@flipkart.com>
2013-12-13 15:23:04 +0530

    method payzippy added

--- app/code/community/FlipKart/PayZippy/Model/System/Config/Source/Payment/Methods.php
+++ app/code/community/FlipKart/PayZippy/Model/System/Config/Source/Payment/Methods.php
@@ -17,11 +17,11 @@ class FlipKart_PayZippy_Model_System_Config_Source_Payment_Methods
     public function toOptionArray()
     {
         $methods = array();
+		$methods[] = array('value' => 'PAYZIPPY' ,'label' => 'PayZippy');
         $methods[] = array('value' => 'CREDIT' ,'label' => 'Credit Card');
         $methods[] = array('value' => 'DEBIT' ,'label' => 'Debit Card');
         $methods[] = array('value' => 'EMI' ,'label' => 'Credit Card EMI');
         $methods[] = array('value' => 'NET' ,'label' => 'Net Banking');
-        
         return $methods;
     }
 }
