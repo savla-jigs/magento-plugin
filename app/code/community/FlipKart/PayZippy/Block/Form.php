152bea57ee3ee4b1f2e11ffd2f13331061098af6
savla-jigs <savla.jigs@gmail.com>
2015-03-27 19:28:54 +0530

    Update Form.php
    Removing commented code

--- app/code/community/FlipKart/PayZippy/Block/Form.php
+++ app/code/community/FlipKart/PayZippy/Block/Form.php
@@ -16,44 +16,5 @@ class FlipKart_PayZippy_Block_Form extends Mage_Payment_Block_Form {
                 Mage::helper('payzippy')->__('You will be redirected to the PayZippy website when you place an order.')
             );
     }
-
-    /*
-    *Return all allowed payment methods for payzippy like EMI,Debit Card etc
-    */
-
-    // public function getAllowedMethods() {
-    // 	$available_methods = array();
-    // 	$methods  = Mage::getStoreConfig('payment/payzippy/payment_methods');
-    // 	$methods = explode(',',$methods);
-    // 	$label_codes = Mage::getSingleton('payzippy/system_config_source_payment_methods')->toOptionArray();
-    //     $availables = array();
-    //     foreach($methods as $method) {
-    //         foreach($label_codes as $label) {
-    //             if($label['value'] == $method) {
-    //                 $availables[] = $label;
-    //             }
-    //         }
-    //     }
-    //     return $availables;	
-    // }
-    
-    
-    // *Return allowed bank names for either payment method Net banking or EMI 
-    
-
-    // public function getBankNames($paymentMethod) {        
-    //     $bankCodes = Mage::getStoreConfig('payment/payzippy/'.$paymentMethod);
-    //     $bankCodes =  explode(',', $bankCodes);
-    //     $bankLabels = Mage::getSingleton('payzippy/system_config_source_payment_bank_names')->toOptionArray();
-    //     $availables = "";
-    //     foreach($bankCodes as $code) {
-    //         foreach($bankLabels as $label) {
-    //             if($label['value'] == $code) {
-    //                 $availables.= '<option value="'.$label["value"].'">'.$label["label"].'</option>';
-    //             }
-    //         }
-    //     }
-    //     return $availables;
-    // }
 }
 ?>
